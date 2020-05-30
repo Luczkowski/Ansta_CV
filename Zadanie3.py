5446979ec2e0793b4cb7cca8203543b0157019a9
Maciej Łuczkowski <66076120+Luczkowski@users.noreply.github.com>
2020-05-30 18:56:18 +0200

    Update Zadanie3.py
    one-liner

--- Zadanie3.py
+++ Zadanie3.py
@@ -3,12 +3,4 @@
 from decimal import *
 
 def decimal_list():
-
-    x = 2.0
-    my_list = []
-
-    while x <= 5.5:
-        my_list.append(Decimal(x))
-        x += 0.5
-
-    return my_list
+    return [Decimal(i * 0.5) for i in range(4,12)]
